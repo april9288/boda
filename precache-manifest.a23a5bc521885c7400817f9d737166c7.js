bb45c371acd35b0c9feca7660e29d912587c2b80
Jongho Kim <april9288@gmail.com>
2018-10-11 15:27:23 -0700

    Updates

---
+++ precache-manifest.a23a5bc521885c7400817f9d737166c7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d02ff49e68e7dc97700b",
+    "url": "/boda/static/js/runtime~main.d02ff49e.js"
+  },
+  {
+    "revision": "ccddd4af1ba6998e2420",
+    "url": "/boda/static/js/main.ccddd4af.chunk.js"
+  },
+  {
+    "revision": "a23fe6a98a76b5f06d69",
+    "url": "/boda/static/js/1.a23fe6a9.chunk.js"
+  },
+  {
+    "revision": "ccddd4af1ba6998e2420",
+    "url": "/boda/static/css/main.3f6499d5.chunk.css"
+  },
+  {
+    "revision": "c24a79722320cf30f8d84927cac7be35",
+    "url": "/boda/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
 
 importScripts(
-  "/boda/precache-manifest.dd5a89e57ec77c56e512c81bfa41c1b7.js"
+  "/boda/precache-manifest.a23a5bc521885c7400817f9d737166c7.js"
 );
 
 workbox.clientsClaim();
