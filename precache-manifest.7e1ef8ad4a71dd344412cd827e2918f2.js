b75745a2adc88b857c924f54b443a968f992522d
Jongho Kim <april9288@gmail.com>
2018-10-10 22:53:10 -0700

    Updates

---
+++ precache-manifest.7e1ef8ad4a71dd344412cd827e2918f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d02ff49e68e7dc97700b",
+    "url": "/boda/static/js/runtime~main.d02ff49e.js"
+  },
+  {
+    "revision": "4dbc94ef2bac2e70811f",
+    "url": "/boda/static/js/main.4dbc94ef.chunk.js"
+  },
+  {
+    "revision": "2e796cda0e6e559b91fb",
+    "url": "/boda/static/js/1.2e796cda.chunk.js"
+  },
+  {
+    "revision": "4dbc94ef2bac2e70811f",
+    "url": "/boda/static/css/main.280e7679.chunk.css"
+  },
+  {
+    "revision": "40b0ebd54313b1a1e84fe523723ab7de",
+    "url": "/boda/index.html"
+  }
+];
